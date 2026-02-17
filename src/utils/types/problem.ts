74b168b639d85b662ea414819afccd20b6376334
Umang70 <us1800439@gmail.com>
2026-02-17 13:53:04 +0530

    first commit

---
+++ src/utils/types/problem.ts
@@ -0,0 +1,34 @@
+import type { StaticImageData } from "next/image";
+
+export type Example = {
+	id: number;
+	inputText: string;
+	outputText: string;
+	explanation?: string;
+	img?: StaticImageData;
+};
+
+// local problem data
+export type Problem = {
+	id: string;
+	title: string;
+	problemStatement: string;
+	examples: Example[];
+	constraints: string;
+	order: number;
+	starterCode: string;
+	handlerFunction: ((fn: any) => boolean) | string;
+	starterFunctionName: string;
+};
+
+export type DBProblem = {
+	id: string;
+	title: string;
+	category: string;
+	difficulty: string;
+	likes: number;
+	dislikes: number;
+	order: number;
+	videoId?: string;
+	link?: string;
+};
